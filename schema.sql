6836e2d9ec8ad5eddbf363f50204fdad713eba79
Julian Pluas <julianpluas@gmail>
2021-03-08 18:59:24 -0800

    Created a table for department

--- schema.sql
+++ schema.sql
@@ -2,3 +2,9 @@ DROP DATABASE IF EXISTS employee_DB;
 CREATE database employee_DB;
 
 USE employee_DB;
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (ID)
+);
